3ed6fc42b72ed5490b3038c448a93c0a1d49d981
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-12-16 16:21:52 +0700

    test 2

--- ChatbotApp/test2.py
+++ ChatbotApp/test2.py
@@ -1,10 +1,13 @@
-# from ChatbotApp.dataProcess import data_process
-# rootPath = 'D:\\Reactjs\\HTTT\\Chat-Box-Deep-Learning-\\ChatbotApp'
-# type = 'true'# true or wrong
-# import os
-# train_x,train_y = data_process(rootPath,os.path.abspath('data/'+type+'/trainning/trainning.json'),type)
-import re
-from static.emo_unicode import UNICODE_EMOJI
-emoji_pattern = re.compile('|'.join(UNICODE_EMOJI), flags=re.UNICODE)
-emoji_example = " Without you by my side, 💓 😉 I am not complete. You have given me the best of love, 🎈 and I want to be by your side forever. Thank you for giving my life that direction it needed. 💋‍ Thank you for loving me unconditional. 💏"
-print(emoji_pattern.sub(r'', emoji_example))
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import SGD
+from ChatbotApp.dataProcess import data_process
+import os
+
+rootPath = 'D:\\Reactjs\\HTTT\\Chat-Box-Deep-Learning-\\ChatbotApp'
+type = 'wrong'# true or wrong
+
+train_x,train_y = data_process(rootPath,os.path.abspath('data/'+type+'/trainning/trainning.json'),type)
+print(train_x)
+print(train_y)
