38dff51fe13a8d9fcee395e409698c89f09b5b33
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-12-16 16:03:24 +0700

    update dataset & fixed nltk

--- ChatbotApp/app3.py
+++ ChatbotApp/app3.py
@@ -4,26 +4,42 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
 from flask_cors import CORS
 from keras.models import load_model
-
 import os
+import json
+import random
+import string
+import re
+from static.emo_unicode import UNICODE_EMOJI
+
+url_pattern = re.compile(r'http\S+')
+emoji_pattern = re.compile('|'.join(UNICODE_EMOJI), flags=re.UNICODE)
+
 # rootPath = 'E:\\DoAnPhatTrienHeThongThongMinh\\ChatbotApp'
 type = 'true'# true or wrong
 model = load_model(os.path.abspath('model/'+type+'/model.h5'))
-import json
-import random
+
 # intents = json.loads(open('data.json').read())
 intents = json.loads(open(os.path.abspath('data/'+type+'/trainning/trainning.json'), encoding="utf8").read())
 words = pickle.load(open(os.path.abspath('model/'+type+'/texts.pkl'), 'rb'))
 classes = pickle.load(open(os.path.abspath('model/'+type+'/labels.pkl'), 'rb'))
 
+
 def clean_up_sentence(sentence):
+    # ignore special characters
+    sentence_trans = sentence.translate(str.maketrans('', '', string.punctuation))
+    # ignore link
+    sentence_trans = url_pattern.sub(r'', sentence_trans)
+    # ignore emotions
+    sentence_trans = emoji_pattern.sub(r'', sentence_trans)
     # tokenize the pattern - split words into array
-    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = nltk.word_tokenize(sentence_trans)
+
     # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    print(sentence_words)
+
     return sentence_words
 
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
@@ -31,11 +47,13 @@ def clean_up_sentence(sentence):
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
+
     # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s:
+                print(w)
                 # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
@@ -57,7 +75,6 @@ def predict_class(sentence, model):
 
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
-
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
         if(i['tag']== tag):
@@ -72,22 +89,21 @@ def chatbot_response(msg):
     res = getResponse(ints, intents)
     return res
 
-
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 cors = CORS(app)
 app.static_folder = 'static'
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-@app.route("/get")
 
+@app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
     return chatbot_response(userText)
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
--- ChatbotApp/test2.py
+++ ChatbotApp/test2.py
@@ -1,2 +1,10 @@
-import  os
-print(os.path.abspath("model/true/model.h5"))
+# from ChatbotApp.dataProcess import data_process
+# rootPath = 'D:\\Reactjs\\HTTT\\Chat-Box-Deep-Learning-\\ChatbotApp'
+# type = 'true'# true or wrong
+# import os
+# train_x,train_y = data_process(rootPath,os.path.abspath('data/'+type+'/trainning/trainning.json'),type)
+import re
+from static.emo_unicode import UNICODE_EMOJI
+emoji_pattern = re.compile('|'.join(UNICODE_EMOJI), flags=re.UNICODE)
+emoji_example = " Without you by my side, 💓 😉 I am not complete. You have given me the best of love, 🎈 and I want to be by your side forever. Thank you for giving my life that direction it needed. 💋‍ Thank you for loving me unconditional. 💏"
+print(emoji_pattern.sub(r'', emoji_example))
